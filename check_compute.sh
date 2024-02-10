389cf426441c49ce0b8c50505c8b26f89d5af7fc
AddheWarmanPutra <addhe.warman@gmail.com>
2024-02-10 16:07:21 +0700

    Update check_compute.sh
    this version using logic exclude during gcloud gather list project

--- check_compute.sh
+++ check_compute.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-
 exclude_project="exclude-project-dev exclude-project-stg"
-
-for a in $(gcloud projects list | grep "dev\|stg" | awk '{print $1}')
+for a in $(gcloud projects list --filter="NOT (projectId=$(echo $exclude_project | sed 's/ / OR projectId=/g'))"| grep "dev\|stg" | awk '{print $1}')
 do
-  if [[ ! $exclude_project =~ $a ]]; then
-    echo "Check Project $a...."
+  if [[ $a != $exclude_project ]]; then
+    echo "Checking Project $a..."
     gcloud compute instances list --project $a --filter status=RUNNING
   else
-    echo "Skip Check Project $a."
+    echo "Skip project $a.. it's include in the list"
   fi
 done
